5ec5c360e8099edc35b0a391c3ee8339d6687830
sanjel0307 <sanjel0307@gmail.com>
2021-07-19 10:58:18 +0100

    setting up Kivy
    setting up kivy

---
+++ main.py
@@ -0,0 +1,25 @@
+import kivy 
+from kivy.app import App
+from kivy.uix.label import Label 
+from kivy.uix.gridlayout import GridLayout 
+from kivy.uix.textinput import TextInput 
+from kivy.uix.button import Button 
+from kivy.uix.widget import Widget 
+
+
+class Design(Widget): 
+    pass
+    
+
+
+
+class MyApp(App):
+
+    def build(self):
+        return Design() 
+
+
+if __name__ == "__main__":
+    MyApp().run()
+
+
