0c9023aa22db291c0e83958722f4468bc866b08c
sanjel0307 <sanjel0307@gmail.com>
2021-07-19 11:59:41 +0100

    added required changes for screen navigation

--- main.py
+++ main.py
@@ -5,18 +5,28 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput 
 from kivy.uix.button import Button 
 from kivy.uix.widget import Widget 
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen 
 
 
-class Design(Widget): 
+class MainWindow(Screen):
     pass
-    
 
 
+class SecondWindow(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+
+kv = Builder.load_file("my.kv")
 
 class MyApp(App):
 
     def build(self):
-        return Design() 
+        return kv
 
 
 if __name__ == "__main__":
